31b390559abb03355051dc06a1e4847de18926c7
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-06 13:15:58 -0600

    log

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./components/Auth.js"
+import Body from "./components/Body.js"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/body" element={<Body />} />
 
       </Routes>
     </BrowserRouter>
--- login-page/src/components/Auth.js
+++ login-page/src/components/Auth.js
@@ -5,7 +5,7 @@ export default function (props) {
 
 
         <div className="Auth-form-container">
-            
+
             <form className="Auth-form">
                 <div className="Auth-form-content">
                     <h3 className="Auth-form-title"></h3>
@@ -36,13 +36,12 @@ export default function (props) {
                     <p className="forgot-password text-right mt-2">
                         Forgot <a href="#">password?</a>
                     </p>
+                    
                 </div>
             </form>
-            <body>
-                <div className="out-view-container">
-                    {/* <h1>Hello test</h1> */}
-                </div>
-            </body>
+            
         </div>
+
+        
     )
 }
