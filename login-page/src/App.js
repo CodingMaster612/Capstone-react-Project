89141208ca0a14c99c4b3b955947d912dc3b3da1
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-07 14:13:06 -0600

    log

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,17 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-// import Navbar from './components/Navbar';
 import styled from "styled-components";
 import "./App.css"
-import Home from './components/pages/Home';
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import ContactUs from './components/pages/ContactUs';
-import SignUp from './components/pages/SignUp';
-import Marketing from './components/pages/Marketing';
-import Consulting from './components/pages/Consulting';
-import Auth from "./components/Auth.js"
+
 import { AccountBox } from "./components/accountBox";
+
+
 
 
 
@@ -26,25 +22,24 @@ const AppContainer = styled.div`
 
 function App() {
   return (
-    <AppContainer>
-      
-    <BrowserRouter>
-    {/* <Navbar/> */}
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/home" element={<AccountBox />}/>
-        {/* <Route path='/'component={<Home/>} />
-        <Route path='/services' component={<Services/>} />
-        <Route path='/products' component={<Products/>} />
-        <Route path='/contact-us' component={<ContactUs/>} />
-        <Route path='/sign-up' component={<SignUp/>} />
-        <Route path='/marketing' component={<Marketing/>} />
-        <Route path='/consulting' component={<Consulting/>} /> */}
+    
 
-      </Routes>
-    </BrowserRouter>
+
+    <AppContainer>
+     
+      
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<AccountBox />} />
+          
+
+        </Routes>
+        
+      </BrowserRouter>
     </AppContainer>
     
+
   )
 }
 
